28fd37e73ae2d8adfb12ded9fc992735333596ff
presley972 <presleylupon@gmail.com>
2023-03-19 09:45:50 -0400

    envoie de mail en preprod

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -54,7 +54,7 @@ class RegistrationController extends AbstractController
             );
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('user');
+            return $this->redirectToRoute('login');
         }
 
         return $this->render('registration/register.html.twig', [
